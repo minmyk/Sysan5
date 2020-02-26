7101b8fe2ebe3f99d39d7585733b4859698beba7
pechor1n <mykyta.minenko@gmail.com>
2020-02-26 23:41:54 +0200

    class solver initiated

--- Sysan5.py
+++ Sysan5.py
@@ -1,10 +1,10 @@
-from matplotlib import animation as animation
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from PyQt5.QtWidgets import QSpinBox, QTableWidget, QLabel, QApplication, QLineEdit, QDialog, QGroupBox, \
     QHBoxLayout, QComboBox, QGridLayout, QStyleFactory, QCheckBox, QPushButton, QWidget, QTableWidgetItem, QTabWidget
 from PyQt5.QtGui import QPalette, QColor, QIcon
 import sys
+import re
 from PyQt5.QtCore import Qt
 from functools import reduce
 import numpy as np
@@ -188,6 +188,19 @@ class UI(QDialog):
 
     def execute(self):
         self.useStylePaletteCheckBox.setEnabled(False)
+
+
+class Solver:
+    def __init__(self):
+        pass
+
+    def collect_data(self):
+        file = open('data/sys_lab5.txt', 'r')
+        data = file.read()
+        file.close()
+        for matrix in data.split(';'):
+            yield pd.DataFrame(np.array([el.split() for el in matrix.split('\n') if el]).flatten().reshape(4, 7)).apply(
+                    pd.to_numeric)
 
 
 if __name__ == '__main__':
